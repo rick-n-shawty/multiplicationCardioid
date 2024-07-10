27c17bb78f679f76c65462416806770fe673f875
rick-n-shawty <sultanhoodie@gmail.com>
2024-07-10 14:28:04 +0800

    circumference points

--- include/Cardioid.hpp
+++ include/Cardioid.hpp
@@ -7,11 +7,19 @@ class Cardioid{
         sf::CircleShape mainCircle;
         float factor = 2;
         int base = 10;
+        std::vector<sf::CircleShape> points;
     public: 
         Cardioid(float radius);
         ~Cardioid();
         void show(sf::RenderWindow& window){
             window.draw(mainCircle); 
+            for(int i = 0; i < points.size(); i++){
+                window.draw(points[i]);
+            }
+        }
+        void setBase(int newBase){
+            if(newBase < 2) return; 
+            base = newBase; 
         }
 };
 #endif
--- src/Cardioid.cpp
+++ src/Cardioid.cpp
@@ -6,7 +6,19 @@ Cardioid::Cardioid(float radius){
     mainCircle.setPosition(sf::Vector2f(0,0));
     mainCircle.setFillColor(sf::Color::Transparent);
     mainCircle.setOutlineThickness(2);
-    mainCircle.setPointCount(100);
+    mainCircle.setPointCount(100); 
+    float arcLength = 2 * M_PI * radius / base; 
+    float theta = M_PI;
+    for(int i = 0; i < base; i++){
+        float x = radius * cos(theta); 
+        float y = radius * sin(theta); 
+        points.push_back(sf::CircleShape());
+        points[i].setRadius(5);
+        points[i].setOrigin(sf::Vector2f(5,5));
+        points[i].setPosition(sf::Vector2f(x, y));
+        points[i].setFillColor(sf::Color::Red); 
+        theta -= (arcLength / radius);
+    }
 }; 
 
 Cardioid::~Cardioid(){
